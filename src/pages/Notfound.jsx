463e76619a52be2d370ca915f6ac57c1683f2c8f
Sandra Sarkodie <sandysark02@gmail.com>
2025-06-26 07:38:54 +0000

    last commit

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import image from "../assets/404.jpg.jpg";
+import image from '../assets/notfound.jpg'
 import { Link } from "react-router";
-import Navbar from "../components/Navbar";
 
 export default function NotFound() {
   return (
     <>
-      <Navbar />
       <div className="flex justify-center items-center h-screen px-8">
-        <img src={image} alt="404 page" />
+        <img src={image} alt="notfound page" className="flex w-150 h-120" />
+
       </div>
 
-      <div className="flex justify-center mt-2">
+      <div className='flex justify-center mb-50'>
         <Link to="/">
-          <button className="bg-black hover:bg-gray-400 text-white font-semibold py-2 px-6 rounded-xl shadow-md transition duration-300 items-center cursor-pointer">
+          <button className="bg-black hover:bg-gray-400 text-white font-semibold py-2 px-6 rounded-xl shadow-md transition duration-300 items-center cursor-pointer ">
             Continue shopping
           </button>
         </Link>
+
       </div>
+
+
+
     </>
-  );
+
+  )
 }
